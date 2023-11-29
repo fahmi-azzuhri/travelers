30d71c2b26d4790fcc752f4686ebecc19293df1a
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-11-29 22:16:52 +0700

    feat : add sponsor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Jumbotron from "./components/Jumbotron";
 import NavBar from "./components/NavBar";
+import Sponsor from "./components/Sponsor";
 
 function App() {
   return (
     <>
       <NavBar />
       <Jumbotron />
+      <Sponsor />
     </>
   );
 }
