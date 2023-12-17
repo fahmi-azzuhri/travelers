f5a06f9f6ff9f7cc5cc01e083d757c5317f27952
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-12-17 12:09:13 +0700

    add text in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import header from "../assets/img/header.png";
 const Header = () => {
   return (
-    <header>
-      <img src={header} alt="" className="header-padding lg:w-full" />
-    </header>
+    <>
+      <header>
+        <img src={header} alt="" className="header-padding lg:w-full" />
+        <div className="text-overlay">
+          <h1 className="text-white mt-9 text-6xl font-bold hidden sm:inline md:inline lg:inline xl:inline tracking-wider">
+            Let's travel the world
+          </h1>
+          <p className="pt-5 tracking-wider">
+            Explore destinations, places, and unforgettable experiences
+          </p>
+        </div>
+      </header>
+    </>
   );
 };
 
