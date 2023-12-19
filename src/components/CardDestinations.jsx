42b7770424e873206a99d7ac0c82919e6473ad0d
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-12-19 19:42:54 +0700

    refactoring card

--- src/components/CardDestinations.jsx
+++ src/components/CardDestinations.jsx
@@ -2,28 +2,28 @@ import { Card, CardHeader, CardBody } from "@material-tailwind/react";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa";
 
-export function CardDestinations() {
+export function CardDestinations({ imageUrl, location, rating }) {
   return (
     <Card
       shadow={false}
-      className=" mt-5 relative grid h-[30rem] w-[20rem] max-w-[28rem] items-end justify-center overflow-hidden"
+      className="mt-5 relative grid h-[30rem] w-[20rem] max-w-[28rem] items-end justify-center overflow-hidden"
     >
       <CardHeader
         floated={false}
         shadow={false}
         color="transparent"
-        className="absolute inset-0 m-0 h-full w-full rounded-none bg-[url('https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')] bg-cover bg-center"
+        className={`absolute inset-0 m-0 h-full w-full rounded-none bg-[url('${imageUrl}')] bg-cover bg-center`}
       >
         <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
       </CardHeader>
       <CardBody className="relative flex lg:flex-row flex-col p-3">
         <div className="flex flex-row border-white border rounded-full bg-white px-4 py-2 mb-2 lg:mb-0 lg:mr-2">
           <FaLocationDot className="mt-1 mr-2 text-red-500" />
-          <p className="text-black font-bold">Indonesia</p>
+          <p className="text-black font-bold">{location}</p>
         </div>
         <div className="flex flex-row border-white border rounded-full bg-white px-2 py-2">
           <FaStar className="mt-1 mr-2 text-yellow-500" />
-          <p className="text-black font-bold">5.0</p>
+          <p className="text-black font-bold">{rating}</p>
         </div>
       </CardBody>
     </Card>
--- src/components/Destinations.jsx
+++ src/components/Destinations.jsx
@@ -2,13 +2,37 @@ import React from "react";
 import { CardDestinations } from "./CardDestinations";
 
 const Destinations = () => {
+  const destinations = [
+    {
+      imageUrl:
+        "https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      location: "Indonesia",
+      rating: 5.0,
+    },
+    {
+      imageUrl:
+        "https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      location: "Indonesia",
+      rating: 5.0,
+    },
+    {
+      imageUrl:
+        "https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      location: "Indonesia",
+      rating: 5.0,
+    },
+  ];
   return (
     <div className="pt-5 container">
       <div className="justify-between flex flex-row">
         <h1 className="text-2xl font-bold">Popular destinations</h1>
         <span className="text-blue-400">Lihat semua</span>
       </div>
-      <CardDestinations />
+      <div className="flex lg:flex-row flex-col">
+        {destinations.map((destination, index) => (
+          <CardDestinations key={index} {...destination} />
+        ))}
+      </div>
     </div>
   );
 };
